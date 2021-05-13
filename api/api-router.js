a9c127c2b2b4a449f44c5a27e87692e6cbfff14f
Tony Sorensen <tony48853@gmail.com>
2021-05-12 20:12:12 -0700

    test

--- api/api-router.js
+++ api/api-router.js
@@ -7,35 +7,36 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const messageOfTheDay = process.env.MOTD || "Welcome to my API Server";
+  res.status(200).json({ messageOfTheDay, api: "up" });
 });
 
 router.get("/shouts", (req, res, next) => {
   Shouts.find()
-    .then(shouts => {
+    .then((shouts) => {
       res.status(200).json(shouts);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 router.post("/shouts", (req, res, next) => {
   Shouts.add(req.body)
-    .then(shout => {
+    .then((shout) => {
       res.status(201).json(shout);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 router.delete("/shouts/:id", (req, res) => {
   Shouts.remove(req.params.id)
-    .then(count => {
+    .then((count) => {
       if (count) {
         res.status(204).end();
       } else {
         res.status(404).json({ message: "not found" });
       }
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 router.use(errorHandler);
