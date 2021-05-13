3c79981baf2daba5df50a16430afe344b693a065
Tony Sorensen <tony48853@gmail.com>
2021-05-12 19:36:40 -0700

    another test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
+const pw = process.env.SECRET_PASSWORD;
+console.log("hi", port, pw);
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
